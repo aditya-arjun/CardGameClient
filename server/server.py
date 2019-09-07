19083d3a3f3569b45b68541e895425f23c5c33e7
aditya-arjun <aditya.arjun123@gmail.com>
2019-09-07 07:59:24 -0400

    add server basic code

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_socketio  import SocketIO, join_room, emit
+
+app = Flask('card_app')
+socketio = SocketIO(app)
+rooms = {}
+
+@socketio.on('create')
+def on_create(data):
+    print('hello')
+
+# https://secdevops.ai/weekend-project-part-2-turning-flask-into-a-real-time-websocket-server-using-flask-socketio-ab6b45f1d896

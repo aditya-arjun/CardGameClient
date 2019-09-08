7ef626397aabaae38905d7887977f31d3d573ac1
Richard Guo <richgtx@gmail.com>
2019-09-08 03:16:36 -0400

    lots of updates

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -84,35 +84,35 @@ function getInitialCards() {
         name: "JC",
         x: 150,
         y: 250,
-        faceUp: true,
+        faceUp: false,
         owner: null
     },
     {
         name: "3D",
-        x: 100,
-        y: 200,
-        faceUp: true,
+        x: 150,
+        y: 250,
+        faceUp: false,
         owner: null
     },
     {
         name: "4D",
-        x: 100,
-        y: 200,
-        faceUp: true,
+        x: 150,
+        y: 250,
+        faceUp: false,
         owner: null
     },
     {
         name: "3C",
-        x: 100,
-        y: 200,
-        faceUp: true,
+        x: 150,
+        y: 250,
+        faceUp: false,
         owner: null
     },
     {
         name: "5D",
-        x: 100,
-        y: 200,
-        faceUp: true,
+        x: 150,
+        y: 250,
+        faceUp: false,
         owner: null
     }
 ]
@@ -167,10 +167,12 @@ function createPersonalHand(arr) {
     }
 }
 
-function initializePlayers(players) {
+function initializePlayers(players, playerImages) {
     let positions = initializePositions(players.length-1, 1000);
     let idx = 0;
-    for (let player of players) {
+    for (let i = 0; i < players.length; i++) {
+        let player = players[i];
+        //let playerImage = playerImages[i];
         if (player == username) {
             continue;
         }
@@ -180,7 +182,15 @@ function initializePlayers(players) {
         let playerShape = new createjs.Shape();
         playerShape.graphics.beginFill("rgba(2, 136, 209, 0.25)").drawEllipse(0, 0, 100, 100);
         playerShape.regX = -50;
-        playerContainer.addChild(playerShape);
+        let image = document.createElement("img");
+        image.crossOrigin = "Anonymous";
+        image.src = playerImages[i];
+        let playerImage = new createjs.Bitmap(image);
+        playerImage.crossOrigin = "Anonymous";
+        playerImage.scale = .2;
+        playerImage.mask = playerShape;
+        playerImage.x = 50;
+        playerContainer.addChild(playerShape, playerImage);
         //let playerName = new createjs.Text(player, "20px Arial");
         //let playerCardCount = new createjs.Text("0", "20px Arial");
         //playerCardCount.y = 30;
@@ -218,6 +228,8 @@ function initializeCursors(players) {
         if (player == username) continue;
         let cursor = new createjs.Shape();
         cursor.graphics.beginStroke(cursorColor).drawEllipse(0, 0, 10, 10);
+        cursor.x = -10;
+        cursor.y = -10;
         stage.addChild(cursor);
         cursorsDict[player] = cursor;
     }
@@ -248,7 +260,12 @@ function initializeCards(initialCards) {
         image.scale = .15;
         let cardback = new createjs.Bitmap(preloader.getResult("cardback"));
         cardback.scale = .15;
-        cardContainer.addChild(image);
+        if (card.faceUp) {
+            cardContainer.addChild(image);
+        }
+        else {
+            cardContainer.addChild(cardback);
+        }
         let bounds = image.getBounds();
         cardContainer.regX = bounds.width*image.scale/2;
         cardContainer.regY = bounds.height*image.scale/2;
@@ -290,12 +307,13 @@ function init() {
 
     let initialCards = getInitialCards();
     let players = getAllPlayers();
-    let cursorColor = "#32CD32";
+    cursorColor = "#32CD32";
+    let playerImages = ["https://lh4.googleusercontent.com/-DeP3OFe72UA/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rc7FWra7Ou9HgqSdwI_kqf-2FSzGQ/photo.jpg", "https://lh4.googleusercontent.com/-DeP3OFe72UA/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rc7FWra7Ou9HgqSdwI_kqf-2FSzGQ/photo.jpg", "https://lh4.googleusercontent.com/-DeP3OFe72UA/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rc7FWra7Ou9HgqSdwI_kqf-2FSzGQ/photo.jpg"]
     preloader.loadManifest(initializeImages(initialCards));
     preloader.on("complete", e => handleFileComplete(initialCards, players));
     username = getUserName();
     
-    initializePlayers(players);
+    initializePlayers(players, playerImages);
     initializeCursors(players);
 
     function makeResponsive(isResp, respDim, isScale, scaleType) {
@@ -365,14 +383,14 @@ function handleFileComplete(initialCards, players) {
     //createPersonalHand(["5D", "3C"]);
 
     let dealButton = new createjs.DOMElement("deal-button");
-    dealButton.x = 41;
-    dealButton.y = 160;
-    dealButton.scale = .5;
+    dealButton.x = 55/ PIXEL_RATIO;
+    dealButton.y = 200 / PIXEL_RATIO;
+    dealButton.scale = .5 / PIXEL_RATIO;
     stage.addChild(dealButton);
     let resetButton = new createjs.DOMElement("reset-button");
-    resetButton.x = 41;
-    resetButton.y = 180;
-    resetButton.scale = .5;
+    resetButton.x = 55 / PIXEL_RATIO;
+    resetButton.y = 220 / PIXEL_RATIO;
+    resetButton.scale = .5 / PIXEL_RATIO;
     stage.addChild(resetButton);
     document.getElementById("deal-button").addEventListener("click", e => sendDealEvent());
     document.getElementById("reset-button").addEventListener("click", e => sendResetEvent());
@@ -412,8 +430,6 @@ function handleFileComplete(initialCards, players) {
     stage.on("stagemousemove", e => {
         let mouseX = e.stageX / scalingRatio;
         let mouseY = e.stageY / scalingRatio;
-        /*cursor.x = mouseX;
-        cursor.y = mouseY;*/
         sendMoveCursor(username, mouseX, mouseY);
     });
     stage.update();
